552e1d4f3142f327f14453266af3878e63338b9f
LuvneeshM <lm3300@nyu.edu>
2019-03-03 23:51:36 -0500

    removed the config file

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	consumer_key:         '3B3kKdIUfAErGbd8BKjlBqtmv',
-	consumer_secret:      'zOiGERAxgbrJEkG3ZM1teEvuo8Q3KQ4YLXkKvmoT8eWctEQMJm',
-	access_token:         '1098735479145594880-TOgu4LOqrNGdu9vBjmPInw3QUsGoJr',
-	access_token_secret:  'tURtXtYsd9wADSWqOyROGkX9GJmTfDxbJaj6DV5MF9Fgg',
-	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-	strictSSL:            true     // optional - requires SSL certificates to be valid.
-}
